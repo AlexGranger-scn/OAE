2870afe049e82fc43f4b3540a2789773c0bd1371
Maruko <3123767595@qq.com>
2023-03-07 17:14:24 +0800

    add test

---
+++ test/model.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import torch.nn.init as init
+import numpy as np
+
+
+class OrderedAutoEncoder(nn.Module):
+    def __init__(self, hidden_layer_size):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(256,hidden_layer_size, bias=None)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(hidden_layer_size, 256, bias=None)
+        )
+
+        self.tanh = nn.Tanh()
+        
+        for m in self.modules():
+            if m.__class__ == nn.Conv2d or m.__class__ == nn.Linear:
+                init.xavier_normal_(m.weight.data)
+
+    def forward(self, x):
+        #encoder
+        hidden_layer =self.encoder(x)
+        
+        #decoder
+        decoded = self.decoder(hidden_layer)
+        
+        return hidden_layer, decoded
